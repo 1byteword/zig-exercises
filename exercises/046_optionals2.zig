afb0f3245ed156f9bbc63625073e49f78d8d2b3f
Azhan Khan <1byteword@gmail.com>
2024-08-21 19:44:17 -0500

    init

--- exercises/046_optionals2.zig
+++ exercises/046_optionals2.zig
@@ -22,7 +22,7 @@ const std = @import("std");
 
 const Elephant = struct {
     letter: u8,
-    tail: *Elephant = null, // Hmm... tail needs something...
+    tail: *Elephant = null.?, // Hmm... tail needs something...
     visited: bool = false,
 };
 
@@ -37,8 +37,8 @@ pub fn main() void {
 
     // `linkElephants` will stop the program if you try and link an
     // elephant that doesn't exist! Uncomment and see what happens.
-    // const missingElephant: ?*Elephant = null;
-    // linkElephants(&elephantC, missingElephant);
+    const missingElephant: ?*Elephant = null;
+    linkElephants(&elephantC, missingElephant);
 
     visitElephants(&elephantA);
 
@@ -66,6 +66,6 @@ fn visitElephants(first_elephant: *Elephant) void {
 
         // HINT: We want something similar to what `.?` does,
         // but instead of ending the program, we want to exit the loop...
-        e = e.tail ???
+        e = e.tail orelse break;
     }
 }
